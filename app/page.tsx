2efbc8a22d00eaa5b14a3a56bb82ebd7474b7dfd
George <xsova113@gmail.com>
2023-05-19 00:21:50 -0700

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="sm:max-w-[55%] sm:h-[60%] md:max-w-[45%] md:h-[55%] max-w-[80%] h-[55%]  bg-gradient-to-t from-gray-900 to-gray-600 pt-5 px-8 rounded-3xl">
+      <div className="sm:max-w-[55%] sm:h-[60%] md:max-w-[45%] md:h-[55%] max-w-[90%] h-[60%]  bg-gradient-to-t from-gray-900 to-gray-600 pt-5 px-8 rounded-3xl">
         {isSubmitted ? (
           <ThankState rating={rating} />
         ) : (
